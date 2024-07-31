31cd690678e30d2e3783dd36f17e8bcccb1bb432
IkerCasana <casanaiker007@gmail.com>
2024-07-30 21:49:19 -0600

    poop

--- pandas read.py
+++ pandas read.py
@@ -1,8 +1,7 @@
-import pandas as pd
 import openpyxl
+import pandas as pd
 import numpy as np
 
 data = pd.read_excel(r'C:\Users\milob\OneDrive\Escritorio\PLANETS_WITH_ESI.xlsm')
-
 data_array = np.array(data)
 print(data_array)

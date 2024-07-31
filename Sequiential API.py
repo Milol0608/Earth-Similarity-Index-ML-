22a082888e4242dca72713d288aae90219c1b562
Milol0608 <74789039+Milol0608@users.noreply.github.com>
2024-07-30 20:35:15 -0600

    panda read

--- Sequiential API.py
+++ Sequiential API.py
@@ -6,6 +6,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
 
+
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(-1, 28*28).astype('float32') / 255.0
 x_test = x_test.reshape(-1, 28*28).astype('float32') / 255.0
@@ -28,3 +29,4 @@ model.compile(
 )
 model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
 model.evaluate(x_test, y_test, batch_size=32, verbose=2)
+
---
+++ pandas read.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import openpyxl
+
+data = pd.read_excel(r'C:\Users\milob\OneDrive\Escritorio\NASA EXOPLANETS Corrected Version.xlsm')
+print(data)

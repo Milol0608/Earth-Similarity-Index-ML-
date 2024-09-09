699523da646b1dccaf21fc95982d2d3321a7649e
Milol0608 <74789039+Milol0608@users.noreply.github.com>
2024-09-09 07:50:04 -0600

    Update pandas read.py

--- pandas read.py
+++ pandas read.py
@@ -2,7 +2,7 @@ import openpyxl
 import pandas as pd
 import numpy as np
 
-data = pd.read_excel(r'C:\Users\milob\OneDrive\Escritorio\PLANETS_WITH_ESI.xlsm')
+data = pd.read_excel(r'C:\Users\File_Location) #Change to file location
 data_array = np.array(data)
 print(data_array)
 print(data_array.shape)
